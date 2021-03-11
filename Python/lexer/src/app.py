1547d6f06f768af82cf1115ff3f530ced3b469f3
wendrew <wendrew.soares@concil.com.br>
2021-03-11 17:10:32 -0300

    implementando os tokens faltantes

--- Python/lexer/src/app.py
+++ Python/lexer/src/app.py
@@ -7,14 +7,11 @@ class Token(object):
         A simple Token structure.
         Contains the token type, value and position.
     """
-    def __init__(self, type, val, line, pos):
-        self.type = type
+    def __init__(self, val):
         self.val = val
-        self.line = line
-        self.pos = pos
 
     def __repr__(self):
-        return f'Token(type: {self.type}, value: {self.val!r}, line: {self.line}, position: {self.pos})'
+        return f'{self.val!r}'
 
 
 class LexerError(Exception):
@@ -26,26 +23,15 @@ class LexerError(Exception):
         self.pos = pos
 
 
-#TODO: Implementar As classes na execução do codigo
-#TODO: Obter uma lista de caracteres e separar caracter por caracter
-# '<[2{12.5 6.0}](3 -4 5)>'
-# ['<', '[', 2, '{', 12.5, 6.0, '}', ']', '(', 3, -4, 5, ')', '>']
-
 if __name__ == "__main__":
     while True:
         phrase = input('Digite um charactere ou frase: ')
-        pos = phrase.index(phrase)
-        tipo = type(phrase)
-        line = phrase.count('\n')
 
         try:
             for k, v in tokens:
                 if k[0] in phrase:
                     print(Token(
-                        type=tipo,
                         val=f'{k[0]} => {v}',
-                        line=line,
-                        pos=pos
                     ))
         except LexerError as error:
             print(error)
--- Python/lexer/src/tokens.py
+++ Python/lexer/src/tokens.py
@@ -1,11 +1,10 @@
-#TODO: Implementar tokens
 tokens = [
-    ('{', 'left-key'),
-    ('}', 'right-key'),
-    ('[', 'left-brackets'),
-    (']', 'right-brackets'),
-    ('(', 'left-parentheses'),
-    (')', 'right-parentheses'),
+    ('{', 'LKEY'),
+    ('}', 'RKEY'),
+    ('[', 'LBRACKETS'),
+    (']', 'RBRACKETS'),
+    ('(', 'LPARENTHESES'),
+    (')', 'RPARENTHESES'),
     ('+', 'PLUS'),
     ('-', 'MINUS'),
     ('/', 'DIVISION'),
@@ -17,46 +16,80 @@ tokens = [
     ('<=', 'LTE'),
     ('>=', 'GTE'),
     ('==', 'DOUBLEEQUAL'),
-    ('!=', 'NE'),
-]
+    ('&', 'AND'),
+    ('|', 'OR'),
+    ('int', 'INTEGER'),
+    ('float', 'FLOAT'),
+    ('str', 'STRING'),
+    ('#', 'COMMENT'),
 
-#tokens = [
-#                                                     # OPERATORS #
-#'PLUS' ,        # +
-#'MINUS' ,       # -
-#'MULTIPLY',     # *
-#'DIVIDE',       # /
-#'MODULO',       # %
-#
-#
-#'NOT',          # ~
-#'EQUALS',       # =
-#
-#                                                     # COMPARATORS #
-#'LT',           # <
-#'GT',           # >
-#'LTE',          # <=
-#'GTE',          # >=
-#'DOUBLEEQUAL',  # ==
-#'NE',           # !=
-#'AND',          # &
-#'OR' ,          # |                                                
-#                                                      # BRACKETS #
-#
-#'LPAREN',       # (
-#'RPAREN',       # )
-#'LBRACE',       # [
-#'RBRACE',       # ]
-#'BLOCKSTART',   # {
-#'BLOCKEND',     # }
-#                                                    # DATA TYPES#
-#
-#'INTEGER',      # int
-#'FLOAT',       # dbl
-#
-#'COMMENT',  # --
-#
-#]
+    # capital
+    ('A', 'CHAR a'),
+    ('B', 'CHAR b'),
+    ('C', 'CHAR c'),
+    ('D', 'CHAR d'),
+    ('E', 'CHAR e'),
+    ('F', 'CHAR f'),
+    ('G', 'CHAR g'),
+    ('H', 'CHAR h'),
+    ('I', 'CHAR i'),
+    ('J', 'CHAR j'),
+    ('K', 'CHAR k'),
+    ('L', 'CHAR l'),
+    ('M', 'CHAR m'),
+    ('N', 'CHAR n'),
+    ('O', 'CHAR o'),
+    ('P', 'CHAR p'),
+    ('Q', 'CHAR q'),
+    ('R', 'CHAR r'),
+    ('S', 'CHAR s'),
+    ('T', 'CHAR t'),
+    ('U', 'CHAR u'),
+    ('Y', 'CHAR y'),
+    ('V', 'CHAR v'),
+    ('X', 'CHAR x'),
+    ('W', 'CHAR w'),
+    ('Y', 'CHAR y'),
+    ('Z', 'CHAR z'),
+
+    # tiny
+    ('a', 'CHAR a'),
+    ('b', 'CHAR b'),
+    ('c', 'CHAR c'),
+    ('d', 'CHAR d'),
+    ('e', 'CHAR e'),
+    ('f', 'CHAR f'),
+    ('g', 'CHAR g'),
+    ('h', 'CHAR h'),
+    ('i', 'CHAR i'),
+    ('j', 'CHAR j'),
+    ('k', 'CHAR k'),
+    ('l', 'CHAR l'),
+    ('m', 'CHAR m'),
+    ('n', 'CHAR n'),
+    ('o', 'CHAR o'),
+    ('p', 'CHAR p'),
+    ('q', 'CHAR q'),
+    ('r', 'CHAR r'),
+    ('s', 'CHAR s'),
+    ('t', 'CHAR t'),
+    ('u', 'CHAR u'),
+    ('v', 'CHAR v'),
+    ('x', 'CHAR x'),
+    ('w', 'CHAR w'),
+    ('y', 'CHAR y'),
+    ('z', 'CHAR z'),
+    ('0', 'INT zero'),
+    ('1', 'INT one'),
+    ('2', 'INT two'),
+    ('3', 'INT three'),
+    ('4', 'INT four'),
+    ('5', 'INT five'),
+    ('6', 'INT six'),
+    ('7', 'INT seven'),
+    ('8', 'INT eight'),
+    ('9', 'INT nine'),
+]
 
 # Regular expression rules for simple tokens
 
